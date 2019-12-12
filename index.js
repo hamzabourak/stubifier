81bf1cdd10c78cd3c358de6ca795ae1dd7fdfe9f
hamzabourak <hamza.bourak@nymity.com>
2019-12-12 14:52:45 -0500

    Remove scope

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const channel = new BroadcastChannel('stubify_channel');
 
-exports.stub = function(serviceWorkerUrl, fakeEndpoints, scope){
+exports.stub = function(serviceWorkerUrl, fakeEndpoints){
     if (!('serviceWorker' in navigator)) {
         return;
     }
     window.addEventListener('load', function() {
-        navigator.serviceWorker.register(serviceWorkerUrl, {scope: scope || '/'}).then(function(registration) {
+        navigator.serviceWorker.register(serviceWorkerUrl).then(function(registration) {
             console.log('ServiceWorker registration successful with scope: ', registration.scope);
             channel.postMessage(fakeEndpoints);
         }, function(err) {
